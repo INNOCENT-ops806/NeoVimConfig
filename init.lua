05a02231ef12a26009294ec355cc804fa5fc4ee8
INNOCENT-ops806 <240418541@tut4life.ac.za>
2025-07-05 21:36:45 +0200

    add todo shortcut and some themes

--- init.lua
+++ init.lua
@@ -4,5 +4,8 @@ require("config.lazy")
 --vim.cmd "colorscheme habamax"
 --vim.cmd("colorscheme slate")
 --vim.cmd "colorscheme evening"
---vim.cmd "colorscheme vim"
+--vim.cmd("colorscheme vim")
 vim.cmd("colorscheme retrobox")
+--vim.cmd("colorscheme tokyonight")
+--vim.cmd("colorscheme peachpuff")
+vim.keymap.set("n", "<leader>todo", ":e ~/notes/todos.md<CR>", { desc = "Open Todo list" })
