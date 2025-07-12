8ba2abfce7aa90a892440cac95747ab03eb0ebfb
INNOCENT-ops806 <240418541@tut4life.ac.za>
2025-07-12 02:11:43 +0200

    refactor: Disable arrow keys for improved navigation training

--- init.lua
+++ init.lua
@@ -10,3 +10,11 @@ vim.cmd("colorscheme retrobox")
 --vim.cmd("colorscheme peachpuff")
 vim.keymap.set("n", "<leader>todo", ":e ~/notes/todos.md<CR>", { desc = "Open Todo list" })
 vim.keymap.set("n", "<leader>s", ":nohlsearch<CR>", { desc = "Clear search highlights" })
+
+-- break old habits
+vim.keymap.set({ "n", "v", "i" }, "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "i" }, "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "i" }, "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "i" }, "<Right>", "<Nop>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<BS>", "<Nop>", { noremap = true, silent = true })
