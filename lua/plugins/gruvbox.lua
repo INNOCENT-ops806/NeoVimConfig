363c859363a932e1594ac7e9bb843736e1845775
INNOCENT-ops806 <240418541@tut4life.ac.za>
2025-08-20 16:15:09 +0200

    Feat: add gruvbox colorscheme

---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      --vim.g.gruvbox_contrast_dark = "soft"
+      --vim.g.gruvbox_contrast_dark = "medium"
+      vim.g.gruvbox_contrast_dark = "hard"
+      vim.g.gruvbox_italic = 1
+    end,
+  },
+}
