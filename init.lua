abfe6569587f1a2b69ae995b6d184cd9d26f512f
INNOCENT-ops806 <240418541@tut4life.ac.za>
2025-10-31 15:50:31 +0200

    feat: Update

--- init.lua
+++ init.lua
@@ -1,12 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 --vim.cmd "colorscheme elflord"
-vim.cmd("colorscheme habamax")
+--vim.cmd("colorscheme habamax")
 --vim.cmd("colorscheme slate")
 --vim.cmd "colorscheme evening"
 --vim.cmd("colorscheme vim")
 --vim.cmd("colorscheme default")
 --vim.cmd("colorscheme retrobox")
+vim.cmd("colorscheme tokyonight-night")
 --vim.cmd("colorscheme tokyonight")
 --vim.cmd("colorscheme rose-pine")
 --vim.cmd("colorscheme OceanicNext")
@@ -16,7 +17,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.keymap.set("n", "<leader>todo", ":e ~/notes/todos.md<CR>", { desc = "Open Todo list" })
-vim.keymap.set("n", "<leader>s", ":nohlsearch<CR>", { desc = "Clear search highlights" })
+--vim.keymap.set("n", "<leader>s", ":nohlsearch<CR>", { desc = "Clear search highlights" })
 
 -- break old habits
 vim.keymap.set({ "n", "v", "i" }, "<Up>", "<Nop>", { noremap = true, silent = true })
